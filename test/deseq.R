3cd11849fd89af829a1057ae3c0a64d004fc7f33
Douglas Wu <wckdouglas@gmail.com>
2018-04-23 13:30:51 -0500

    added deseq test

---
+++ test/deseq.R
@@ -0,0 +1,26 @@
+#!/usr/bin/env Rscript
+
+library(DESeq2)
+
+df <- read.table('data/ercc.tsv', header=T) 
+
+sample_df <- data.frame(samplename = names(df)[2:ncol(df)])
+sample_df$sample <- substr(sample_df$samplename, 1,1)
+sample_df$batch <- substr(sample_df$samplename, 3,3)
+row.names(sample_df) <- sample_df$samplename
+sample_df$sample <- factor(sample_df$sample)
+
+
+count_matrix <- subset(df, , -id)
+row.names(count_matrix) <- df$id
+dds <- DESeqDataSetFromMatrix(countData = count_matrix,
+                       colData = sample_df, 
+                       design = ~ sample)
+dds <- DESeq(dds)
+res <- results(dds)
+res$id = row.names(res)
+write.table(data.frame(res), 
+            file = 'data/R_deseq.tsv', 
+            sep='\t', 
+            row.names=F,
+            quote=F)
---
+++ test/test_deseq.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+import os
+test_data_path = os.path.dirname(os.path.realpath(__file__)) + '/data'
+
+
+def test_deseq():
+    import pandas as pd 
+    import numpy as np
+    from diffexp.py_deseq import py_DESeq2
+
+    df = pd.read_table(test_data_path + '/ercc.tsv')
+    """
+        id     A_1     A_2     A_3     B_1     B_2     B_3
+    0  ERCC-00002  111461  106261  107547  333944  199252  186947
+    1  ERCC-00003    6735    5387    5265   13937    8584    8596
+    2  ERCC-00004   17673   13983   15462    5065    3222    3353
+    3  ERCC-00009    4669    4431    4211    6939    4155    3647
+    4  ERCC-00012       0       2       0       0       0       0
+    """
+
+
+    sample_df = pd.DataFrame({'samplename': df.columns}) \
+        .query('samplename != "id"')\
+        .assign(sample = lambda d: d.samplename.str.extract('([AB])_', expand=False)) \
+        .assign(batch = lambda d: d.samplename.str.extract('_([123])', expand=False)) 
+    sample_df.index = sample_df.samplename
+
+    dds = py_DESeq2(count_matrix = df,
+               design_matrix = sample_df,
+               design_formula = '~ sample',
+               gene_column = 'id')
+    
+    dds.run_deseq() 
+    dds.get_deseq_result()
+    res = dds.deseq_result 
+    res.query('padj < 0.05')
+    assert(res.query('padj < 0.05').shape == (43,7))
+
+    res.to_csv(test_data_path + '/py_deseq.tsv', index=False, sep='\t')
