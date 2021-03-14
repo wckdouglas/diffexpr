d4a9f0760a9770daaf316cf9c485526311e2c760
Douglas Wu <wckdouglas@gmail.com>
2021-03-14 10:36:30 -0400

    better docs

--- diffexpr/py_deseq.py
+++ diffexpr/py_deseq.py
@@ -18,22 +18,28 @@ class py_DESeq2:
     '''
     DESeq2 object through rpy2
 
-    input:
-    count_matrix: should be a pandas dataframe with each column as count, and a id column for gene id
-        example:
+    Args:
+        count_matrix (pd.DataFrame): should be a pandas dataframe with each column as count, and a id column for gene id
+        design_matrix (pd.DataFrame): an design matrix in the form of pandas dataframe, see DESeq2 manual, samplenames as rownames
+        design_formula (str): see DESeq2 manual, example: "~ treatment""
+        gene_column (str): column name of gene id columns, example "id"
+
+
+    count_matrix example::
+
         id    sampleA    sampleB
         geneA    5    1
         geneB    4    5
         geneC    1    2
-    design_matrix: an design matrix in the form of pandas dataframe, see DESeq2 manual, samplenames as rownames
-                treatment
-    sampleA1        A
-    sampleA2        A
-    sampleB1        B
-    sampleB2        B
 
-    design_formula: see DESeq2 manual, example: "~ treatment""
-    gene_column: column name of gene id columns, exmplae "id"
+    Design matrix example:: 
+
+                    treatment
+        sampleA1        A
+        sampleA2        A
+        sampleB1        B
+        sampleB2        B
+
     '''
     def __init__(self, count_matrix, design_matrix, design_formula, gene_column='id'):
         try:
@@ -61,20 +67,47 @@ class py_DESeq2:
 
 
     def run_deseq(self, **kwargs):
+        """
+        actually running deseq2
+
+        Args:
+            **kwargs: Any keyword arguments for DESeq
+
+        From DESeq2 manual:
+
+        DESeq(
+            object,
+            test = c("Wald", "LRT"),
+            fitType = c("parametric", "local", "mean", "glmGamPoi"),
+            sfType = c("ratio", "poscounts", "iterate"),
+            betaPrior,
+            full = design(object),
+            reduced,
+            quiet = FALSE,
+            minReplicatesForReplace = 7,
+            modelMatrixType,
+            useT = FALSE,
+            minmu = if (fitType == "glmGamPoi") 1e-06 else 0.5,
+            parallel = FALSE,
+            BPPARAM = bpparam()
+        )
+        """
         self.dds = deseq.DESeq(self.dds, **kwargs)
+        self.comparison = list(deseq.resultsNames(self.dds))
 
 
     def get_deseq_result(self, contrast=None, **kwargs):
         '''
         DESeq2: result(dds, contrast)
+        making a dds.deseq_result pandas dataframe
         '''
 
-        self.comparison = list(deseq.resultsNames(self.dds))
         if contrast:
             if len(contrast)==3:
                 R_contrast = robjects.vectors.StrVector(np.array(contrast)) 
             else:
-                assert len(contrast) == 2, 'Contrast must be length of 3 or 2'
+                if len(contrast) != 2
+                    raise ValueError('Contrast must be length of 3 or 2')
                 R_contrast = robjects.ListVector({None:con for con in contrast})
             logger.info('Using contrast: %s' %contrast)
             self.result = deseq.results(self.dds, contrast = R_contrast, **kwargs) # Robject
@@ -87,7 +120,8 @@ class py_DESeq2:
 
     def normalized_count(self):
         '''
-        return Counts(dds, normalized=TRUE)
+        Returns:
+            pd.DataFrame: a dataframe in the format of DESeq2::Counts(dds, normalized=TRUE) in R
         '''
         normalized_count_matrix = deseq.counts_DESeqDataSet(self.dds, normalized=True)
         normalized_count_matrix = to_dataframe(normalized_count_matrix)
@@ -104,6 +138,13 @@ class py_DESeq2:
         see: http://bioconductor.org/packages/devel/bioc/vignettes/DESeq2/inst/doc/DESeq2.html
 
         Be sure to check dds.comparison to see which coef (1-base) to use
+
+        Args:
+            coef (int): 1-based index for selecting which of dds.comparison to show
+            method (str): DESeq2 lfcshrink method ("apeglm", "ashr", "normal")
+
+        Returns:
+            pandas.DataFrame: a deseq2 result table
         '''
         lfc = deseq.lfcShrink(self.dds, res = self.result, coef = coef, type = method)
         with localconverter(robjects.default_converter + pandas2ri.converter):
