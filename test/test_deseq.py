d1dff46383d1c7b049bfd3129b2e08b4fe95b8cd
Douglas Wu <wckdouglas@gmail.com>
2022-12-18 22:47:04 -0500

    better test

--- test/test_deseq.py
+++ test/test_deseq.py
@@ -37,8 +37,8 @@ def sample_metadata():
         .query('samplename != "id"')
         .assign(sample=lambda d: d["samplename"].str.extract("([AB])_", expand=False))
         .assign(batch=lambda d: d["samplename"].str.extract("_([123])", expand=False))
+        .pipe(lambda d: d.set_index(d["samplename"]))
     )
-    sample_df.index = sample_df["samplename"]
     return sample_df
 
 
