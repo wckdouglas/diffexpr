995b7c50cb1dd815755a35bf9a5ab92bf2c1bc02
Douglas Wu <wckdouglas@gmail.com>
2023-01-27 22:04:39 -0500

    isntall hdf5

--- setup.R
+++ setup.R
@@ -1,7 +1,7 @@
 install.packages(c('BiocManager','Hmisc', 'RcppEigen','RcppNumerical'), 
                  dependencies='Depends',
                 repo = "http://cran.us.r-project.org")
-BiocManager::install(c('DESeq2','apeglm', 'rhdf5'))
+BiocManager::install(c('DESeq2','apeglm', 'rhdf5', 'tximport'))
 
 
 library(DESeq2)
